c08f0999520127cdfffb0b655d169e615deb24f5
MohdAadil01 <mohdaadil4240@gmail.com>
2024-03-02 00:12:18 +0530

    Add Avatar

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,10 +13,51 @@ import React, { useState } from "react";
 import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
 
 function Login() {
+  // PAGE SWITCH
   const [isLoginPage, setIsLoginPage] = useState(true);
   const pageSwitchHandler = () => {
     setIsLoginPage((prev) => !prev);
   };
+  // for login
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameChangeHandler = (e) => {
+    setUsername(e.target.value);
+  };
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+  };
+  const loginHandler = (e) => {
+    e.preventDefault();
+  };
+
+  // let isValidUserName = false;
+  // for register
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [profileSrc, setProfileSrc] = useState(null);
+
+  const nameChangeHandler = (event) => {
+    setName(event.target.value);
+  };
+  const bioChangeHandler = (event) => {
+    setBio(event.target.value);
+  };
+  const profilePicChangeHandler = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProfileSrc(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  const signUpHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div
       style={{
@@ -52,6 +93,7 @@ function Login() {
                   width: "100%",
                   marginTop: "1rem",
                 }}
+                onSubmit={loginHandler}
               >
                 <TextField
                   required
@@ -60,7 +102,14 @@ function Login() {
                   type="normal"
                   margin="normal"
                   variant="outlined"
+                  value={username}
+                  onChange={usernameChangeHandler}
                 ></TextField>
+                {/* {!isValidUserName && (
+                  <Typography color="error" variant="caption">
+                    username is not valid
+                  </Typography>
+                )} */}
                 <TextField
                   required
                   fullWidth
@@ -68,6 +117,8 @@ function Login() {
                   type="password"
                   margin="normal"
                   variant="outlined"
+                  value={password}
+                  onChange={passwordChangeHandler}
                 ></TextField>
                 <Button
                   variant="contained"
@@ -103,6 +154,7 @@ function Login() {
                   width: "100%",
                   marginTop: "1rem",
                 }}
+                onSubmit={signUpHandler}
               >
                 <Stack position={"relative"} width={"10rem"} margin={"auto"}>
                   <Avatar
@@ -111,6 +163,7 @@ function Login() {
                       height: "10rem",
                       objectFit: "contain",
                     }}
+                    src={profileSrc}
                   ></Avatar>
                   <IconButton
                     sx={{
@@ -127,7 +180,10 @@ function Login() {
                   >
                     <>
                       <CameraAlt />
-                      <VisuallyHiddenInput type="file" />
+                      <VisuallyHiddenInput
+                        type="file"
+                        onChange={profilePicChangeHandler}
+                      />
                     </>
                   </IconButton>
                 </Stack>
@@ -139,6 +195,8 @@ function Login() {
                   // type="normal"
                   margin="normal"
                   variant="outlined"
+                  value={name}
+                  onChange={nameChangeHandler}
                 ></TextField>
                 <TextField
                   required
@@ -146,6 +204,8 @@ function Login() {
                   label="Bio"
                   margin="normal"
                   variant="outlined"
+                  value={bio}
+                  onChange={bioChangeHandler}
                 ></TextField>
                 <TextField
                   required
@@ -154,6 +214,8 @@ function Login() {
                   // type="normal"
                   margin="normal"
                   variant="outlined"
+                  value={username}
+                  onChange={usernameChangeHandler}
                 ></TextField>
                 <TextField
                   required
@@ -162,6 +224,8 @@ function Login() {
                   type="password"
                   margin="normal"
                   variant="outlined"
+                  value={password}
+                  onChange={passwordChangeHandler}
                 ></TextField>
                 <Button
                   variant="contained"
